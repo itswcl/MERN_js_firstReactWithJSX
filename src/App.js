0169277d08fddb68a660f40e030ed73370d1c90c
weicheng lee <z82111712@gmail.com>
2022-01-10 17:31:51 -0800

    update first react with JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+// run npm install react-bootstrap bootstrap@5.1.3 and add line below
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+      </div>
+
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+
+      <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+
+      <script
+        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+        crossorigin></script>
+
+      <script
+        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin></script>
+
+      <script>var Alert = ReactBootstrap.Alert;</script>
     </div>
+
   );
 }
 
